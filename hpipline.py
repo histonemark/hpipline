ef4e2f0d5541310a242c48bcc49638119d7e8bff
Ruggero Cortini <ruggero.cortini@crg.eu>
2018-05-08 10:49:53 +0200

    removed hardcoded paths in pipeline

--- hpipline.py
+++ hpipline.py
@@ -1,10 +1,34 @@
 #!/usr/bin/env python
 # -*- coding:utf-8 -*-
 
-# Python path for the cluster
+import gzip
 import sys
-sys.path.append("/software/gfilion/seeq/")
-sys.path.append("/software/gfilion/gzopen/")
+import seeq
+
+class gzopen(object):
+    def __init__(self, fname):
+        f = open(fname)
+        magic_number = f.read(2)
+        f.seek(0)
+        if magic_number == '\x1f\x8b':
+            self.f = gzip.GzipFile(fileobj=f)
+        else:
+            self.f = f
+            def __enter__(self):
+                return self
+   def __exit__(self, type, value, traceback):
+       try:
+           self.f.fileobj.close()
+       except AttributeError:
+           pass
+       finally:
+           self.f.close()
+           def __getattr__(self, name):
+               return getattr(self.f, name)
+   def __iter__(self):
+       return iter(self.f)
+   def next(self):
+       return next(self.f)
 
 # Standard library packages.
 import os
